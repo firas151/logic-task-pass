a374ce5c35359a8c36878441287a115d44470790
firas151 <firas.salih151@gmail.com>
2022-08-15 19:43:59 +0300

    Fixed the equial part

--- Python Testing.py
+++ Python Testing.py
@@ -2,7 +2,7 @@ Meow = input('number input:\n')
 
 if 5 > int(Meow):
  print('Five is greater than given!')
-elif 5==Meow:
-    print('Five is equial to giving')
+elif 5==int(Meow):
+ print('Five is equial to givien')
 else:
  print ('given is bigger')
